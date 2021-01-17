7c10293c6a58a35be355588b6f6aed5bcfb2bc77
01naoya01 <sunny97643182@gmail.com>
2021-01-18 02:17:11 +0900

    start

---
+++ src/test/java/com/example/vol6/Vol6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vol6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vol6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
