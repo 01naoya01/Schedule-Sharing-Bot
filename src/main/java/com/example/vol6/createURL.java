28743ccb9682858f75027a47cc8b68c467ced1b9
kenji <kenji.0210.ku@gmail.com>
2021-01-24 04:02:49 +0900

    createurl fixed #4

--- src/main/java/com/example/vol6/createURL.java
+++ src/main/java/com/example/vol6/createURL.java
@@ -7,12 +7,12 @@ public class createURL {
         String yotei[] = text.split("\n", 0);
         String yotei_name = yotei[0];
         String yotei_start_time = yotei[1].replaceAll("\\s","T");
-        yotei_start_time = yotei_start_time.replaceAll("\\D", "");
+        yotei_start_time = yotei_start_time.replaceAll("\\W", "");
         String yotei_end_time = yotei[2].replaceAll("\\s","T");
-        yotei_end_time = yotei_end_time.replaceAll("\\D", "");
+        yotei_end_time = yotei_end_time.replaceAll("\\W", "");
         String action = "TEMPLATE";
         String url_yotei = "http://www.google.com/calendar/event?action="
-        + action + "&text=" + yotei_name + "&dates=" + yotei_start_time + yotei_end_time;
+        + action + "&text=" + yotei_name + "&dates=" + yotei_start_time + "00/" + yotei_end_time + "00";
 		return url_yotei;
 	}
 }
