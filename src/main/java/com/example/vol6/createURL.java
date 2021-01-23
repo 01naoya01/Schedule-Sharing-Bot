ebcaf3c2132096783788b2edb0eb9819c55f61fa
kenji <kenji.0210.ku@gmail.com>
2021-01-24 04:11:29 +0900

    createurl fixed #4

--- src/main/java/com/example/vol6/createURL.java
+++ src/main/java/com/example/vol6/createURL.java
@@ -5,10 +5,10 @@ public class createURL {
     public static String get(String text) {
         System.out.println("createURLのtext : " + text + "\n");
         String yotei[] = text.split("\n", 0);
-        String yotei_name = yotei[0];
-        String yotei_start_time = yotei[1].replaceAll("\\s","T");
+        String yotei_name = yotei[1];
+        String yotei_start_time = yotei[2].replaceAll("\\s","T");
         yotei_start_time = yotei_start_time.replaceAll("\\W", "");
-        String yotei_end_time = yotei[2].replaceAll("\\s","T");
+        String yotei_end_time = yotei[3].replaceAll("\\s","T");
         yotei_end_time = yotei_end_time.replaceAll("\\W", "");
         String action = "TEMPLATE";
         String url_yotei = "http://www.google.com/calendar/event?action="
